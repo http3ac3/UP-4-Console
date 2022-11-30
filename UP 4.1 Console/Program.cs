eef7897de78cd1cda930647d41df4b411e7dc673
http3ac3 <ura19092004@gmail.com>
2022-11-30 11:23:10 +0300

    Добавьте файлы проекта.

---
+++ UP 4.1 Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UP_4._1_Console
+{
+    internal class Program
+    {
+        static int NOD(int a, int b)
+        {
+            if (a > b) return NOD(a - b, b);
+
+            else if (a < b) return NOD(a, b - a);
+            else return a;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Введите целое положительное число a: ");
+            while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+            {
+                Console.WriteLine("Введены недопустимые значения! Попробуйте снова!");
+                Console.Write("Введите целое положительное число а: ");
+            }
+
+            Console.Write("Введите целое положительное число b: ");
+            while (!int.TryParse(Console.ReadLine(), out b) || b <= 0)
+            {
+                Console.WriteLine("Введены недопустимые значения! Попробуйте снова!");
+                Console.Write("Введите целое положительное число b: ");
+            }
+            
+            Console.WriteLine($"Наибольший общий делитель для чисел {a} и {b}: " + NOD(a, b));
+        }
+    }
+}
---
+++ UP 4.2 Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UP_4._2_Console
+{
+    internal class Program
+    {
+        static void Function (int n)
+        {
+            if (n == 0) 
+                return;
+            else 
+                Function(n - 1);
+
+            Console.Write(n + " ");
+        }
+        static void Main(string[] args)
+        {
+            int N;
+            Console.Write("Введите число N, большее или равное 1000: ");
+            while (!int.TryParse(Console.ReadLine(), out N) || N < 1000)
+                Console.Write("Введены некорректные данные! Введите N (>= 1000) еще раз: ");
+
+            Console.WriteLine();
+            Function(N);
+        }
+    }
+}
